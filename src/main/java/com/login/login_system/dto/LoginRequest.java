08439f63bdc6ffe7b2230e20ec2506b48ac57207
Leonardo <leorod2005@gmail.com>
2024-08-16 16:42:56 -0300

    feat: create register and login DTO

---
+++ src/main/java/com/login/login_system/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.login.login_system.dto;
+
+public record LoginRequest(String email, String password) {
+}
---
+++ src/main/java/com/login/login_system/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.login.login_system.dto;
+
+import com.login.login_system.entities.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank(message = "Name cannot be empty or null")
+        @Size(min = 4, max = 40, message = "Name must be between 6 and 40 characters")
+        String name,
+        @NotBlank(message = "Email cannot be empty or null")
+        @Email(message = "Invalid email")
+        String email,
+        @NotBlank(message = "Password cannot be empty or null")
+        @Size(min = 8, message = "Password must contain at least 8 characters")
+        String password,
+        @NotBlank(message = "Role cannot be empty or null")
+        UserRole role) {
+}
